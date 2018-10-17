c74bf15a3e0edc0800d55d1c3d91e423683862c1
Khanh <Khanh@MacBook-Pro.local>
2018-10-18 03:08:00 +0900

    Initialized project

---
+++ SelfManagement/SelfManagement/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelfManagement
+//
+//  Created by Khanh on 18/10/2018.
+//  Copyright © 2018 Khanh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
